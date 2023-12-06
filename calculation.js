d152fb715d8cf2fbe0bd9fc3658cc727e43d63ae
jonathan juining <73358104+FriesKnight@users.noreply.github.com>
2023-12-07 02:27:40 +0800

    Add files via upload

---
+++ calculation.js
@@ -0,0 +1,17 @@
+// Data from the table 1
+var A5 = 2;
+var A7 = 5;
+var A12 = 3;
+var A13 = 90;
+var A15 = 80;
+var A20 = 28;
+
+// Calculate values for Table 2
+var alphaValue = A5 + A20;
+var betaValue = A15 / A7;
+var charlieValue = A13 * A12;
+
+// Display values in Table 2
+document.getElementById("alphaValue").innerText = alphaValue;
+document.getElementById("betaValue").innerText = betaValue;
+document.getElementById("charlieValue").innerText = charlieValue;
